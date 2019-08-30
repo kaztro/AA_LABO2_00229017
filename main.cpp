7722ea163ee7acb773f71740692523888cd5f70d
kaztro <00229017@uca.edu.sv>
2019-08-30 14:16:51 -0600

    Just working on it

--- main.cpp
+++ main.cpp
@@ -38,16 +38,47 @@ class Stack {
                 }
         }
 
-        int size() {
-            int size = 0;
+        //Discrima los pares y retorna un arreglo con los mismos...
+        Stack getPairs(int sizeS) {
+            Stack stack;
+            if(sizeS == 0) return stack;
             Node *temp = start;
+
             if (!start) cout << "The Stack is empty" << endl;
-            else
-                while(temp) {
-                    size++;
+            else {
+                while (temp) {
+                    if (temp->value % 2 == 0) stack.push(temp -> value);
                     temp = temp -> next;
                 }
-                return size;
+            }
+            return stack;
+        }
+
+    //Discrima los impares y retorna un arreglo con los mismos...
+    Stack getOdds(int sizeS) {
+        Stack stack;
+        if(sizeS == 0) return stack;
+        Node *temp = start;
+        if (!start) cout << "The Stack is empty" << endl;
+        else {
+            while (temp) {
+                if (temp -> value % 2 != 0) stack.push(temp -> value);
+                temp = temp -> next;
+            }
+        }
+        return stack;
+    }
+
+        int size() {
+            int sizeS = 0;
+            Node *temp = start;
+            if (!start) return 0;
+            else {
+                while (temp) {
+                    sizeS++;
+                    temp = temp -> next;
+                }
+            } return sizeS;
         }
 
         Stack() {
@@ -55,67 +86,47 @@ class Stack {
         }
 };
 
-void operateS(int *array, int tamA, int aux, Stack pair, Stack odd) {
+int fillS(int tamA, int aux, Stack stack) {
     if (aux < tamA) {
-        if(array[aux]%2 == 0) {
-            pair.push(array[aux]);
-        } else {
-            odd.push(array[aux]);
-        }
-        operateS(array, tamA, aux + 1, pair, odd);
+        int value = 0;
+        cout << "Enter the " << aux + 1 << " value of " << tamA << endl;
+        cin >> value;
+        stack.push(value);
+        fillS(tamA, aux + 1, stack);
     } else {
-        cout << "Pair stack (contains:" << pair.size() << "): " << endl;
-        pair.showStack();
+        stack.showStack();
         cout << endl;
-        cout << "Odd stack (contains:" << odd.size() << "): " << endl;
-        odd.showStack();
-        cout << endl;
-        if (pair.size() == odd.size()) cout << "Both stacks are equals" << endl;
-        else {
-            if(pair.size() > odd.size()) cout << "The pair stack is bigger" << endl;
-            else cout << "The odd stack is bigger" << endl;
-        }
     }
 }
 
-int fillA(int *array, int tamA, int aux, Stack pair, Stack odd) {
-    if (aux < tamA) {
-        cout << "Enter the " << aux + 1 << " value of " << tamA << endl;
-        cin >> array[aux];
-        fillA(array, tamA, aux + 1);
-    } else operateS(array, tamA, 0, pair, odd);
-}
-
 int main() {
-    int length;
-
-    Stack pair, odd;
+    int length, sSize;
+    Stack principal;
 
     cout << "How many numbers are you gonna enter?" << endl;
     cin >> length;
 
-    int values[length];
+    fillS(length, 0, principal);
 
-    fillA(values, length, 0, pair, odd);
+    sSize = principal.size();
 
-/*
-    do {
-        cout << "Enter an integer, enter any letter or symbol to finish: " << endl;
-        ok = scanf("%d",  &value);
-        (value%2 == 0) ? pair.push(value) : odd.push(value);
+    cout << sSize << endl;
+
+    Stack pair = principal.getPairs(sSize);
+    Stack odd = principal.getOdds(sSize);
+
+    cout << "Pair stack (contains: " << pair.size() << " elements): " << endl;
+    pair.showStack();
+    cout << endl;
+    cout << "Odd stack (contains: " << odd.size() << " elements): " << endl;
+    odd.showStack();
+    cout << endl;
+    if (pair.size() == odd.size()) cout << "Both stacks are equals" << endl;
+    else {
+        (pair.size() > odd.size()) ? cout << "The pair stack is bigger" :
+        cout << "The odd stack is bigger";
+        cout << endl;
     }
-    while(!isdigit(ok));
 
-    while(flag) {
-        cout << "Enter an integer, enter any letter or symbol to finish: " << endl;
-        scanf("%d",  &value);
-        if(flag) {
-            if(value%2 == 0) pair.push(value);
-            else odd.push(value);
-        } else {
-            break;
-        }
-    }
-*/
     return 0;
 }
